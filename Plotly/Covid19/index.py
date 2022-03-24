90fbef01a901f180ec7091ff19c5e889232fed8a
ethanlee <seungwoonlee90@gmail.com>
2022-03-24 19:02:50 +0900

    Plotly & Covid19 dash

---
+++ Plotly/Covid19/index.py
@@ -0,0 +1,15 @@
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
+url_deaths = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+url_recovered = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv'
+
+confirmed = pd.read_csv(url_confirmed)
+deaths = pd.read_csv(url_deaths)
+recovered = pd.read_csv(url_recovered)
+
+print('url_recovered')

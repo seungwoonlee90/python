0118f2c10dcacc683f01978eeedfd6ced48f599b
ethanlee <seungwoonlee90@gmail.com>
2022-03-24 21:31:10 +0900

    Create card sections

--- Plotly/Covid19/index.py
+++ Plotly/Covid19/index.py
@@ -27,6 +27,7 @@ covid_data = covid_data.merge(recovered, how='left', on=['Province/State', 'Coun
 covid_data['date'] = pd.to_datetime(covid_data['date'])
 covid_data['recovered'] = covid_data['recovered'].fillna(0)
 covid_data['active'] = covid_data['confirmed'] - covid_data['death'] - covid_data['recovered']
+covid_data1 = covid_data.groupby(['date'])[['confirmed', 'death', 'recovered', 'active']].sum().reset_index()
 
 app = dash.Dash(__name__, )
 app.layout = html.Div([
@@ -46,8 +47,35 @@ app.layout = html.Div([
         html.Div([
             html.H6('Last Updated: ' + str(covid_data['date'].iloc[-1].strftime('%B %d, %Y') + ' 00:01 (UTC)'))
         ], className='one-third column', id='title1')
+    ], id='header', className='row flex-display'),
+    
+    html.Div([
+        html.Div([
+            html.H6(children='Global cases'),
+            html.P(f"{covid_data1['confirmed'].iloc[-1]:,.0f}"),
+            html.P(f"New : {round(covid_data1['confirmed'].iloc[-1] - covid_data1['confirmed'].iloc[-2], 3):,.0f} 🔺")
+        ], className='card-container three columns'),
         
-    ], id='header', className='row flex-display')
+        html.Div([
+            html.H6(children='Global deths'),
+            html.P(f"{covid_data1['death'].iloc[-1]:,.0f}"),
+            html.P(f"New : {round(covid_data1['death'].iloc[-1] - covid_data1['death'].iloc[-2], 3):,.0f} 🔺")
+        ], className='card-container three columns'),
+        
+        html.Div([
+            html.H6(children='Global recovered'),
+            html.P(f"{covid_data1['recovered'].iloc[-1]:,.0f}"),
+            html.P(f"New : {round(covid_data1['recovered'].iloc[-1] - covid_data1['recovered'].iloc[-2], 3):,.0f} 🔺")
+        ], className='card-container three columns'),
+        
+        html.Div([
+            html.H6(children='Global active'),
+            html.P(f"{covid_data1['active'].iloc[-1]:,.0f}"),
+            html.P(f"New : {round(covid_data1['active'].iloc[-1] - covid_data1['active'].iloc[-2], 3):,.0f} 🔺")
+        ], className='card-container three columns'),
+    ], className='row flex display')
+    
+    
 ], id='main-container')
 
 if __name__ == '__main__' :

d61d46905da786dbc91fc8d6c033bacd0bbbef45
ethanlee <seungwoonlee90@gmail.com>
2022-03-26 14:00:48 +0900

    chained callbacks

--- Plotly/Terrorism/index.py
+++ Plotly/Terrorism/index.py
@@ -18,16 +18,41 @@ app.layout = html.Div([
     ], id="header"),
     html.Div([
         html.Div([
-            html.P("Select Region")
+            html.P("Select Region"),
+            dcc.Dropdown(id='w_region',
+                         multi=False,
+                         searchable=True,
+                         value='East Asia',
+                         placeholder='Select Region',
+                         options=[{'label': c, 'value': c} for c in (df['region_txt'].unique())], className="dropdown")
         ], className="create_container"),
         html.Div([
-            html.P("box 2")
+            html.P("Select Country"),
+            dcc.Dropdown(id='w_country',
+                         multi=False,
+                         searchable=True,
+                         placeholder='Select Country',
+                         options=[], className="dropdown")
         ], className="create_container"),
         html.Div([
             html.P("box 3")
         ], className="create_container")
     ], id="second-container")
 ], id="main-container")
+
+
+@app.callback(Output('w_country', 'options'),
+              [Input('w_region', 'value')])
+def select(w_region):
+    terr = df[df['region_txt'] == w_region]
+    return [{'label': i, 'value': i} for i in terr['country_txt'].unique()]
+
+
+@app.callback(Output('w_country', 'value'),
+              [Input('w_country', 'options')])
+def select(w_country):
+    return [k['value'] for k in w_country][0]
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
